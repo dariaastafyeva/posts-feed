1c01ad395a6880552023a5b61c92770715c6729f
Daria Astafyeva <mermalya17@gmail.com>
2024-05-06 23:22:48 +0300

    added SinglePostPage and post editing functional

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -11,6 +11,15 @@ const postsSlice = createSlice({
     reducers: {
         postAdded(state, action) {
             state.push(action.payload)
+        },
+        postUpdated(state,action){
+            const {id, title, content} = action.playload
+            const existingPost = state.find(post => post.id === id)
+
+            if(existingPost){
+                existingPost.title = title
+                existingPost.content = content
+            }
         }
     }
 })
