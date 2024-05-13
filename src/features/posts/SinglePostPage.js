f655d01f5961844d7a77bdd787226f6b07111459
Daria Astafyeva <mermalya17@gmail.com>
2024-05-13 22:42:29 +0300

    added edit post functionale, post's author, date etc

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
+
     const { postId } = match.params
 
     const post = useSelector(state => 
@@ -21,6 +25,11 @@ export const SinglePostPage = ({ match }) => {
           <article className="post">
               <h2>{post.title}</h2>
               <p className="post-content">{post.content}</p>
+              <PostAuthor userId={post.user} />
+              <TimeAgo timestamp={post.date} />
+              <Link to={`/editPost/${postId}`} className="button">
+                Edit Post
+              </Link>
           </article>
       </section>
   )
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = [
     { id: '1', title: 'First Post!', content: 'Hello!' },
@@ -9,14 +9,28 @@ const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        postAdded(state, action) {
-            state.push(action.payload)
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload)
+            },
+            prepare(title, content, userId) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        date: new Date().toISOString(),
+                        title,
+                        content,
+                        user: userId
+                    }
+                }
+            }
         },
-        postUpdated(state,action){
-            const {id, title, content} = action.playload
+
+        postUpdated(state, action) {
+            const { id, title, content } = action.payload
             const existingPost = state.find(post => post.id === id)
 
-            if(existingPost){
+            if (existingPost) {
                 existingPost.title = title
                 existingPost.content = content
             }
@@ -24,6 +38,6 @@ const postsSlice = createSlice({
     }
 })
 
-export const { postAdded } = postsSlice.actions
+export const { postAdded, postUpdated } = postsSlice.actions
 
 export default postsSlice.reducer
